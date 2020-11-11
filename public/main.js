8526245016cc98d786c35ec64a08bd2606d3f3b0
grant4e <grantfourie00@gmail.com>
2020-11-11 00:00:59 -0500

    very basic static express app

---
+++ public/main.js
@@ -0,0 +1,6 @@
+window.onload = function(e){
+    document.getElementsByClassName("color-button")[0]
+    .addEventListener("click", (e) => {
+        document.body.style.backgroundColor = "#111111"
+    })
+}
