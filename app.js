bcf9c2b26b929f7060e16a4127f1b11c1020afb0
grant4e <grantfourie00@gmail.com>
2020-11-11 17:11:27 -0500

    adding https to node app.js

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express")
+const https = require("https")
+const fs = require("fs")
 app = express()
+
+const sslOptions = {
+    key: fs.readFileSync("/etc/letsencrypt/live/www.squadified.com/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/www.squadified.com/fullchain.pem")
+}
 
 app.use(express.static("public"))
 
-app.listen("80", () => {
-    console.log("listening on port 80")
+app.get("/ajax", (req, res) => {
+    res.writeHead(200)
+    res.end("hello world")
 })
+
+app.listen(8000, () => {
+    console.log("listening on port 8000")
+})
+
+https.createServer(options, app).listen(8080)
