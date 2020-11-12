e6fb906f0e43eaaa1abae0f1446aef19361b5707
grant4e <grantfourie00@gmail.com>
2020-11-12 15:10:39 -0500

    new spotify auth flow

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(passport.session())
 
 
 
-app.get("/", isLoggedIn, (req, res) =>{
+app.get("/me", isLoggedIn, (req, res) =>{
     res.send(`Good morning spotify user: ${req.user.displayName}`)
 })
 
