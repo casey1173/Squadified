9a429a8bae0233cc2c9bd13f815129f984175efc
grant4e <grantfourie00@gmail.com>
2020-11-12 20:43:00 -0500

    index page with request ability

--- public/index.js
+++ public/index.js
@@ -1,16 +1,22 @@
-const currToken = await fetch("https://www.squadified.com/token")
 
-const spotifyAPI = axios.create({
-    method: "get",
-    baseURL: "https://api.spotify.com/v1",
-    headers: {"Authorization" : "Bearer " + currToken}
-})
 
-window.onload = () => {
+
+
+window.onload = async () => {
     const typeField = document.getElementById("type-field")
     const idField = document.getElementById("id-field")
     const result = document.getElementById("result")
     const butt = document.getElementById("request-button")
+    
+
+    const currToken = await fetch("https://www.squadified.com/token")
+
+    const spotifyAPI = axios.create({
+        method: "get",
+        baseURL: "https://api.spotify.com/v1",
+        headers: {"Authorization" : "Bearer " + currToken}
+    })
+
     butt.addEventListener("click", async (e) => {
         const apiRes = await spotifyAPI.get(`/${typeField.value}/${idField.value}`)
         console.log(apiRes)
