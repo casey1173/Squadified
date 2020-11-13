ca84fffa0c7d41bbbd5c2406607a295b7cc1a3f2
grant4e <grantfourie00@gmail.com>
2020-11-12 23:29:25 -0500

    working clientside api

--- public/index.js
+++ public/index.js
@@ -1,19 +1,20 @@
+ currToken = async function(){
+     let res = await axios.get("https://www.squadified.com/token")
+     return res.data
+    }
+
 window.onload = async () => {
     const typeField = document.getElementById("type-field")
     const idField = document.getElementById("id-field")
     const result = document.getElementById("result")
     const butt = document.getElementById("request-button")
     
-    const currToken = await axios.get("https://www.squadified.com/token")
-
-    const spotifyAPI = axios.create({
-        method: "get",
-        baseURL: "https://api.spotify.com/v1",
-        headers: {"Authorization" : "Bearer " + currToken}
-    })
-
     butt.addEventListener("click", async (e) => {
-        const apiRes = await spotifyAPI.get(`/${typeField.value}/${idField.value}`)
+        let apiRes = await axios({
+            method: "get",
+            baseURL: `https://api.spotify.com/v1/${typeField.value}/${idField.value}`,
+            headers: { "Authorization": `Bearer ${await currToken()}` }
+        })
         console.log(apiRes)
     })
 }
