532cffa40ff3130868babf8a8beb5581ce80ac1c
grant4e <grantfourie00@gmail.com>
2020-11-15 12:35:43 -0500

    commit example

---
+++ public/api.js
@@ -0,0 +1,27 @@
+currToken = undefined
+getCurrToken = async function () {
+    if (currToken == undefined || Date.now() >= currToken["expiresAt"]) {
+        let res = await axios.get("https://www.squadified.com/token")
+        currToken = res.data
+        return currToken
+    } else {
+        return currToken
+    }
+}
+
+getResource = async function (path) {
+    return (await axios({
+        method: "get",
+        url: `https://api.spotify.com/v1/` + path,
+        headers: { "Authorization": `Bearer ${(await getCurrToken()).code}` }
+    })).data
+}
+
+getArtists = async function(userName){
+    const playlists = await axios({
+        method: "get",
+        url: `https://users/${userName}/playlists`,
+        headers: {"Authorization": `Bearer ${(await getCurrToken()).code}`}
+    })
+}
+
--- public/index.js
+++ public/index.js
@@ -1,20 +1,12 @@
- currToken = async function(){
-     let res = await axios.get("https://www.squadified.com/token")
-     return res.data
-    }
 
 window.onload = async () => {
     const typeField = document.getElementById("type-field")
     const idField = document.getElementById("id-field")
     const result = document.getElementById("result")
     const butt = document.getElementById("request-button")
-    
+
     butt.addEventListener("click", async (e) => {
-        let apiRes = await axios({
-            method: "get",
-            baseURL: `https://api.spotify.com/v1/${typeField.value}/${idField.value}`,
-            headers: { "Authorization": `Bearer ${await currToken()}` }
-        })
+        let apiRes = getResource(typeField.value + "/" + idField.value)
         console.log(apiRes)
     })
 }
