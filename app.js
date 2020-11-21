a9536e87dfa86712df2ecf158d09c972761be3ec
Casey <caseyhansen@Caseys-MBP.attlocal.net>
2020-11-21 16:13:38 -0500

    trying get

--- app.js
+++ app.js
@@ -121,39 +121,3 @@ app.get("/songs", (req, res) => {
     return storedSongs;    
     //res.json(s);
 });
-
-app.post("/songs", (req, res) => {
-
-    let sIds = (req.data).ids
-    let sNames = (req.data).names
-    let storedSongs = [];
-    let spotifySongs = [];
-    count = 0;
-    sIds.forEach(function(sid) {
-        songData = Song.findByID(sid);
-        if (songData != null) {
-            storedSongs.push(songData.features)
-            count = count+1;
-        } else {
-            spotifySongs.push([sid, sNames[count]]);
-            count = count+1;
-        }
-    })
-    console.log("stored songs: ", storedSongs)
-    console.log("spotify songs: ", spotifySongs)
-    if (spotifySongs != []) {
-        const features = (axios({
-            method: "get",
-            url: "https://api.spotify.com/v1/audio-features",
-            headers: req.headers,
-            params: {"ids": spotifySongs.join(",")}
-        })).data.audio_features
-        Song.createSongs(spotifySongs)
-        Song.addFeatures(spotifySongs, features)
-        storedSongs.push(...features)
-    }
-    console.log("returned features list: ", storedSongs)
-    res.send(storedSongs)
-    return storedSongs;    
-    //res.json(s);
-});
