47bc4aa48d248b7023b4b7ab1ce9dc28d58e58f4
Casey <caseyhansen@Caseys-MBP.attlocal.net>
2020-11-21 19:52:58 -0500

    trying to get song db working

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    watch: '.',
+    ignore_watch : ["Song.js"],
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
